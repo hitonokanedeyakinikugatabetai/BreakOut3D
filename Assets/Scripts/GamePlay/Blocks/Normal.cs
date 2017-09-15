db4f2a1a6c6b847e03282303e964ae2dd7dfb2b7
moso <gootalife0512@gmail.com>
2017-09-15 14:04:26 +0900

    0914ブロック色変更

--- Assets/Scripts/GamePlay/Blocks/Normal.cs
+++ Assets/Scripts/GamePlay/Blocks/Normal.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Normal : Block {
+	void Start() {
+		// 色の変更
+		if ( transform.position.y > 43 ) {
+			GetComponent<Renderer>().material.color = new Color(255f / 255f, 0f / 255f, 0f / 255f);
+		} else if ( transform.position.y > 41.5 ) {
+			GetComponent<Renderer>().material.color = new Color(255f / 255f, 255f / 255f, 0f / 255f);
+		} else if ( transform.position.y > 40 ) {
+			GetComponent<Renderer>().material.color = new Color(0f / 255f, 255f / 255f, 0f / 255f);
+		} else if ( transform.position.y > 38.5 ) {
+			GetComponent<Renderer>().material.color = new Color(0f / 255f, 0f / 255f, 255f / 255f);
+		} else {
+			GetComponent<Renderer>().material.color = new Color(255f / 255f, 0f / 255f, 255f / 255f);
+		}
+	}
 	public Normal() {
 		hp = 1;
 	}
--- Assets/Scripts/GamePlay/GameManager.cs
+++ Assets/Scripts/GamePlay/GameManager.cs
@@ -7,12 +7,14 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 	public GameObject remain; // 残機表示オブジェクト
 	public GameObject gameover; //ゲームオーバー表示オブジェクト
+	public GameObject clear; //ゲームクリア表示オブジェクト
 	public static int rem; // 残機数
 	public static bool ballAlive; // ボールが消えているかどうか
 	public GameObject player; // プレイヤー
 	public GameObject ball; // ボール
 	private BlocksMaker blocksMaker;
 	public int stageNum;
+	public static int blockCt;
 	void Start () {
 		// ステージの番号を設定
 		stageNum = 1;
@@ -21,6 +23,8 @@ public class GameManager : MonoBehaviour {
 		rem = 3; // 残機初期化
 		remain.GetComponent<Text>().text = "ooo"; // 残機表示初期化
 		gameover.GetComponent<Text>().enabled = false; // ゲームオーバーを非表示に初期化
+		clear.GetComponent<Text>().enabled = false; // ゲームクリアを非表示に初期化
+		GameObject.Find("BlockCount").GetComponent<Text>().text = "残り " + GameManager.blockCt.ToString();
 		// プレイヤーとボールを生成
 		Instantiate(player);
 		Instantiate(ball);
@@ -33,6 +37,9 @@ public class GameManager : MonoBehaviour {
 		if ( ballAlive == false && Input.GetKeyDown(KeyCode.B) && rem > -1) {
 			Instantiate(ball);
 			ballAlive = true;
+		}
+		if ( blockCt == 0 ) {
+			clear.GetComponent<Text>().enabled = true; // 表示
 		}
 		//ゲームオーバー
 		if ( rem == -1 ) {
