a80608401d88103affe1fe544c1132121354a6e5
Azreel <garaxy-eyes@docomo.ne.jp>
2017-09-26 18:16:45 +0900

    音とタイトルからの移動

--- Assets/Scripts/Title/Changescene.cs
+++ Assets/Scripts/Title/Changescene.cs
@@ -13,7 +13,7 @@ public class Changescene : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
-			SceneManager.LoadScene("Menu");
+			SceneManager.LoadScene("StageSelect");
 		}
 	}
 }
