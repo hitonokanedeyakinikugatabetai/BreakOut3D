29c608eb35af41e9dd1b4e6ba1c73ac3334eb2c8
codai-04 <s16t236@stu.kagawa-u.ac.jp>
2017-11-22 21:00:56 +0900

    効果音追加

--- Assets/Scripts/GamePlay/Items/WarpZone.cs
+++ Assets/Scripts/GamePlay/Items/WarpZone.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WarpZone : MonoBehaviour {
-	// 10秒間有効
+	private AudioSource[] audioSources; // 以下 効果音
+	private AudioSource warpActivate; // 発動音
+	private AudioSource warpSound; // ワープ音
 	void Start () {
+		// 効果音を追加
+		audioSources = GetComponents<AudioSource>();
+		warpActivate = audioSources[0];
+		warpSound = audioSources[1];
+		AudioSource.PlayClipAtPoint(warpActivate.clip, transform.position);
+		// 10秒間有効
 		Invoke("delete", 10f);
 	}
 	void delete() {
 		Destroy(gameObject);
 	}
-	// ボールを上に移動させる
 	void OnCollisionEnter(Collision col) {
 		if ( col.gameObject.tag == "Ball" ) {
+			// ボールを上に移動させる
   			col.gameObject.transform.position = new Vector3(col.gameObject.transform.position.x, 44f, col.gameObject.transform.position.z);
+			AudioSource.PlayClipAtPoint(warpSound.clip, transform.position);
 		}
 	}
 }
